2f4b85d3156202ce8275713c0484f9f9e6614ab2
SaikySu <140166872+SaikySu@users.noreply.github.com>
2024-03-13 21:29:24 +0700

    Create Buoi1.java
    Add BT1

---
+++ Lý Thuyết/Buoi1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Rectangle {
+    double Chieucao, Chieurong;
+
+    public void getInformation() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println(Chieucao = sc.nextDouble());
+            System.out.println(Chieurong = sc.nextDouble());
+        }
+    }
+
+    public double Chuvi() {
+        return Chieucao * Chieurong;
+    }
+
+    public double Dientich() {
+        return (Chieucao + Chieurong) * 2;
+    }
+
+    public void display() {
+        System.out.println("CHu vi HCN: " + Chuvi());
+        System.out.println("Dien tich HCN: " + Dientich());
+    }
+}
+
+public class HCN {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.getInformation();
+        r.display();
+    }
+}
